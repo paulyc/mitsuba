fcbd9e202d3205da86d2c4c779935c31baea152d
Paul Ciarlo <paul.ciarlo@gmail.com>
2020-01-28 19:29:49 -0500

    yikes well it did finally build

--- envsetup.sh
+++ envsetup.sh
@@ -1,4 +1,8 @@
 export CFLAGS="-DBOOST_ALL_NO_LIB=0 -I/usr/include/glew-1.13.0" #NOPENOPE-I/opt/qt5/include -I/opt/qt5/include
 export CXXFLAGS="-DBOOST_ALL_NO_LIB=0 -I/usr/include/glew-1.13.0" #NOPENOPE-I/opt/qt5/include
+
 #NOPENOPENOPE you have to do these ONLY for the ONE object file and executable that uses QT5 apparently
 #export LDFLAGS=-L/opt/qt5/lib -lQt5XmlPatterns
+
+#ok fine, lol
+ln -sf /usr/include/qt/QtXmlPatterns src/mtsgui/QtXmlPatterns
